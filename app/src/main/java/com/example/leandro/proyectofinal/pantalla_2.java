661f94e96d10ec35de6b08249749997c87035c22
leandroccl3 <leandroccl3@hotmail.com>
2018-02-07 00:23:29 -0300

    Agregué una nueva "activity". Cambié los icono de la facultad y de la universidad de lugar y moví el icono de siguiente en la primer pantalla. El boton de siguiente de la segunda pantalla no redirige a la tercera.

---
+++ app/src/main/java/com/example/leandro/proyectofinal/pantalla_2.java
@@ -0,0 +1,36 @@
+package com.example.leandro.proyectofinal;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class pantalla_2 extends AppCompatActivity {
+    Button atras2;
+    Button siguiente3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_intro);
+
+        atras2 = (Button) findViewById(R.id.btn4);
+        atras2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent atras2 = new Intent(pantalla_2.this, pantalla_intro.class);
+                startActivity(atras2);
+            }
+        });
+        siguiente3 = (Button) findViewById(R.id.btn5);
+        siguiente3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent siguiente_3 = new Intent(pantalla_2.this, pantalla_inicial.class);
+                startActivity(siguiente_3);
+            }
+        });
+
+
+    }
+}
